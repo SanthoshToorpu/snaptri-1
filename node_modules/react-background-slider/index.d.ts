c0ea282b06adb2683d91dc55e344817594a9f9d4
SanthoshToorpu <90833739+SanthoshToorpu@users.noreply.github.com>
2023-06-22 16:42:48 +0530

    work

--- node_modules/react-background-slider/index.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react" />
-
-export interface BackgroundSliderProps {
-    images: string[],
-    duration?: number,
-    transition?: number
-}
-
-export default function BackgroundSlider(props: BackgroundSliderProps): JSX.Element
